7e418845072424e3e13dc5217259abf63f0320f9
Nikita Ghosh <38601527+Nikitaghosh1234@users.noreply.github.com>
2020-08-03 12:58:27 +0530

    Update 1st.py

--- 1st.py
+++ 1st.py
@@ -1,15 +1,16 @@
 import csv,json
 jsonFilePath="happy.json"
 # Read the data from csv
-data={}
+# data={}
 with open('20200617_analytics_v2.csv','r') as csv_file:
-    csv_reader=csv.DictReader(csv_file)
+    csv_reader=csv.reader(csv_file,delimiter=',')
     csv_headings = next(csv_reader)
     first_line = next(csv_reader)
     second_line=next(csv_reader)
-    print(csv_headings)
-    print(first_line)
-    print(second_line)
+   
+    print(csv_headings[4])
+    print(first_line[2])
+    print(second_line[0])
 
     # for row in csv_reader:
     #     # AAAU=row["AAAU"]
